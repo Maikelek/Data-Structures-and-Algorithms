2eaf4da5079f2bba5f9f29d0cb1fdebea48e009a
Marián Zelinka <maikelekmain@gmail.com>
2025-03-20 23:03:07 +0100

    SelectionSort

---
+++ Sorting Algorithms/SelectionSort.js
@@ -0,0 +1,28 @@
+/**
+ * ✅ Selection Sort Algorithm -> sorting an array in ascending order.
+ * The algorithm finds the minimum element in the unsorted part of the array
+ * and swaps it with the first unsorted element. This process is repeated
+ * until the array is sorted.
+ * @param {Array} arr - The array that we want to sort.
+ * @returns {Array} - The sorted array.
+ */
+
+const selectionsort = (arr) => {
+    const arrLength = arr.length;
+    
+    for (let i = 0; i < arrLength - 1; i++) { // Loop through the array
+        let minimalIndex = i; // Assume the first unsorted element is the smallest
+        
+        // Loop through the remaining unsorted elements to find the smallest one
+        for (let j = i + 1; j < arrLength; j++) {
+            if (arr[j] < arr[minimalIndex]) { // If a smaller element is found
+                minimalIndex = j; // Update the index of the smallest element
+            }
+        }
+        
+        // Swap the found minimum element with the first unsorted element
+        [arr[i], arr[minimalIndex]] = [arr[minimalIndex], arr[i]]; 
+    }
+    
+    return arr; // Return the sorted array
+}
